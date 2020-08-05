8c996efcd5c7430d5a1d05b3c360e6aa35b38aa1
Tristan Langford <tristanlangford@hotmail.com>
2020-08-05 11:20:53 +0100

    format a deposit for statement

--- lib/print_statement.rb
+++ lib/print_statement.rb
@@ -6,4 +6,14 @@ class PrintStatement
         @statement = ["date || credit || debit || balance"]
     end
 
+    def format_deposit(account_activity)
+        "#{account_activity.activity_date} || £#{format_currency(account_activity.value)} || || £#{format_currency(account_activity.balance)}"
+    end
+
+    private
+
+    def format_currency(value)
+        '%.2f' % value
+    end
+
 end
--- spec/print_statement_spec.rb
+++ spec/print_statement_spec.rb
@@ -2,14 +2,18 @@ require 'print_statement'
 
 describe PrintStatement do 
 
-    let(:interaction1) { double("account activity", balance: 10, amount: 10) }
-    let(:interaction2) { double("account activity", balance: 8, amount: -2) }
-    let(:interaction3) { double("account activity", balance: 32, amount: 24) }
+    let(:interaction1) { double("account activity", balance: 10, value: 10, activity_date: "06/09/2020") }
+    let(:interaction2) { double("account activity", balance: 8, value: -2, activity_date: "22/10/2020") }
+    let(:interaction3) { double("account activity", balance: 32, value: 24, activity_date: "01/02/2019") }
 
     subject { PrintStatement.new([interaction1, interaction2, interaction3]) }
 
     it 'holds the headers in an array on initialize' do 
         expect(subject.statement).to eq(["date || credit || debit || balance"])
+    end
+
+    it 'formats a deposit output' do 
+        expect(subject.format_deposit(interaction1)).to eq("06/09/2020 || £10.00 || || £10.00")
     end
 
 end
