958fa97d416a5b179f911cf6168def333daaadd3
Tristan Langford <tristanlangford@hotmail.com>
2020-08-05 10:50:20 +0100

    account activity saves time and formats it

--- lib/account_activity.rb
+++ lib/account_activity.rb
@@ -1,10 +1,17 @@
 class AccountActivity
 
-    attr_reader :balance, :value
+    attr_reader :balance, :value, :activity_date
 
     def initialize(balance, value)
         @balance = balance
         @value = value
+        @activity_date = format_date(Time.now)
+    end
+
+    private
+
+    def format_date(date)
+        date.strftime("%d/%m/%Y")
     end
 
 end
--- spec/account_activity_spec.rb
+++ spec/account_activity_spec.rb
@@ -1,8 +1,13 @@
 require 'account_activity'
+require 'timecop'
 
 describe AccountActivity do 
 
     subject { AccountActivity.new(10, 5) } 
+
+    before do
+        Timecop.freeze(Date.parse("11/06/2020"))
+    end
 
     it 'holds the balance' do 
         expect(subject.balance).to eq(10)
@@ -10,6 +15,10 @@ describe AccountActivity do
 
     it 'holds the value by which the balance changed' do 
         expect(subject.value).to eq(5)
+    end
+
+    it 'holds the date it was created' do 
+        expect(subject.activity_date).to eq("11/06/2020")
     end
 
 end
